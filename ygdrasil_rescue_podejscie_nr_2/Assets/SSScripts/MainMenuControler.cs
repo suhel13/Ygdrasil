7f6d8ab975f48294dad740efdcbc484c3e46c21c
suhel13 <39781998+suhel13@users.noreply.github.com>
2018-05-31 04:07:35 +0200

    MainMenu
    Basic menu
    only play button work

---
+++ ygdrasil_rescue_podejscie_nr_2/Assets/SSScripts/MainMenuControler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuControler : MonoBehaviour {
+
+    public GameObject menuPanel;
+
+    public void play()
+    {
+        menuPanel.SetActive(false);
+    }
+
+}
